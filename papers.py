64570d8241865daee1935fc10d6c3ed6d9d6b3bf
belowalo <159521068+belowalo@users.noreply.github.com>
2024-04-02 20:41:23 -0400

    Update papers.py

--- papers.py
+++ papers.py
@@ -1,16 +1,3 @@
-"""Assignment 2: Modelling CS Education research paper data
-
-=== CSC148 Winter 2024 ===
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2024 Bogdan Simion, David Liu, Diane Horton, Jacqueline Smith
-
-"""
 import csv
 from typing import List, Dict
 from tm_trees import TMTree
