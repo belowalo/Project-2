3321271587e9560753bf044884afc6ff19e81bd6
belowalo <159521068+belowalo@users.noreply.github.com>
2024-04-07 19:07:10 -0400

    Add files via upload
    my papers
    !!!

--- papers.py
+++ papers.py
@@ -1,3 +1,64 @@
+"""Assignment 2: Modelling CS Education research paper data
+
+=== CSC148 Winter 2024 ===
+This code is provided solely for the personal and private use of
+students taking the CSC148 course at the University of Toronto.
+Copying for purposes other than this use is expressly prohibited.
+All forms of distribution of this code, whether as given or with
+any changes, are expressly prohibited.
+
+All of the files in this directory and all subdirectories are:
+Copyright (c) 2024 Bogdan Simion, David Liu, Diane Horton,
+                   Haocheng Hu, Jacqueline Smith
+
+=== Module Description ===
+This module contains a new class, PaperTree, which is used to model data on
+publications in a particular area of Computer Science Education research.
+This data is adapted from a dataset presented at SIGCSE 2019.
+You can find the full dataset here: https://www.brettbecker.com/sigcse2019/
+
+Although this data is very different from filesystem data, it is still
+hierarchical. This means we are able to model it using a TMTree subclass,
+and we can then run it through our treemap visualisation tool to get a nice
+interactive graphical representation of this data.
+
+TODO: (Task 6) Complete the steps below
+Recommended steps:
+1. Start by reviewing the provided dataset in cs1_papers.csv. You can assume
+   that any data used to generate this tree has this format,
+   i.e., a csv file with the same columns (same column names, same order).
+   The categories are all in one column, separated by colons (':').
+   However, you should not make assumptions about what the categories are, how
+   many categories there are, the maximum number of categories a paper can have,
+   or the number of lines in the file.
+
+2. Read through all the docstrings in this file once. There is a lot to take in,
+   so don't feel like you need to understand it all the first time.
+   Draw some pictures!
+   We have provided the headers of the initializer as well as of some helper
+   functions we suggest you implement. Note that we will not test any
+   private top-level functions, so you can choose not to implement these
+   functions, and you can add others if you want to for your solution.
+   For this task, we will be testing that you are building the correct tree,
+   not that you are doing it in a particular way. We will access your class
+   in the same way as in the client code in the visualizer.
+
+3. Plan out what you'll need to do to implement the PaperTree initializer.
+   In particular, think about how to use the boolean parameters to do different
+   things in setting up the tree. You may also find it helpful to review the
+   Python documentation about the csv module, which you are permitted and
+   encouraged to use. You should have a good plan, including what your subtasks
+   are, before you begin writing any code.
+
+4. Write the code for the PaperTree initializer and any helper functions you
+   want to use in your design. You should not make any changes to the public
+   interface of this module, or of the PaperTree class, but you can add private
+   attributes and helpers as needed.
+
+5. Tidy and test your code, and try it with the visualizer client code. Make
+   sure you have documented any new private attributes, and that PyTA passes
+   on your code.
+"""
 import csv
 from typing import List, Dict
 from tm_trees import TMTree
@@ -39,7 +100,6 @@ class PaperTree(TMTree):
     _authors: str
     _doi: str
 
-
     def __init__(self, name: str, subtrees: List[TMTree], authors: str = '',
                  doi: str = '', citations: int = 0, by_year: bool = True,
                  all_papers: bool = False) -> None:
@@ -65,7 +125,6 @@ class PaperTree(TMTree):
         super().__init__(name, temp_subtrees, citations)
         self._authors = authors
         self._doi = doi
-
 
     def get_separator(self) -> str:
         """Return the file separator for this Tree.
@@ -97,16 +156,18 @@ def _load_papers_to_dict(by_year: bool = True) -> Dict:
             authors, name, year, temp_categories, doi, citations = row
             categories = temp_categories.split(':')
 
-            if by_year:
-                categories.insert(0, year)
+            # Determine the key to use for nesting
+            key = year if by_year else categories[0]
 
             # Construct the nested dictionary
-            temp_dict = result
-            for category in categories:
-                temp_dict = temp_dict.setdefault(category, {})
+            esteemed_dictionary = result.setdefault(key, {})
+
+            # Traverse nested dictionaries for each category
+            for category in categories if by_year else categories[1:]:
+                esteemed_dictionary = esteemed_dictionary.setdefault(category, {})
 
             # Add paper data to the innermost dictionary
-            temp_dict[name] = {
+            esteemed_dictionary[name] = {
                 'authors': authors,
                 'name': name,
                 'doi': doi,
@@ -119,19 +180,20 @@ def _load_papers_to_dict(by_year: bool = True) -> Dict:
 def _build_tree_from_dict(nested_dict: Dict) -> List[PaperTree]:
     """Return a list of trees from the nested dictionary <nested_dict>.
     """
-    ans = []
-    if nested_dict == {}:
-        return ans
+    def build_tree_helper(node):
+        """Recursive helper function to build the tree."""
+        trees = []
+        for name, child_node in node.items():
+            if 'authors' in child_node.keys():
+                trees.append(PaperTree(name, [], child_node['authors'],
+                                       child_node['doi'], child_node['citations']))
+            else:
+                subtree = build_tree_helper(child_node)
+                trees.append(PaperTree(name, subtree))
+        return trees
 
-    elif 'authors' in nested_dict.keys():
-        ans.append(PaperTree(nested_dict['name'], [], nested_dict['authors'],
-                             nested_dict['doi'], nested_dict['citations']))
-
-    else:
-        for name, yep in nested_dict.items():
-            ans.append(PaperTree(name, _build_tree_from_dict(yep)))
-
-    return ans
+    trees = build_tree_helper(nested_dict)
+    return trees
 
 
 if __name__ == '__main__':
